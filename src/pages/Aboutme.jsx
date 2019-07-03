7410c3e666add683a91bc7eb57ee77dd046eda22
donatelek <47256040+donatelek@users.noreply.github.com>
2019-07-03 18:50:11 +0200

    Adding description about me

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -9,15 +9,16 @@ import nodejs from '../img/nodejs.png'
 import illustrator from '../img/illustrator.png'
 import sass from '../img/sass.png'
 import vsc from '../img/vsc.png'
+import redux from '../img/redux.png'
 import {Roll,Fade} from 'react-reveal'
 
 const Aboutme = () => {
     return ( 
         <section className="Aboutme" >
-          <Roll right cascade>
-          <h1>Few words about me and myself</h1>
-           <div>Hi my name is Jakub Sznajder. Im junior developer looking for job. mmmmm kkkkad adsasdads asdads asdas das as das dasdasdads asd as as dasadsdsas asd asd as as ask mkmkm kmkmkmkmkmk kmmkmk mkmk mk mkmkmkmkmk mkkmk </div>
-           </Roll>
+         
+          <h1>Few Words About Me</h1>
+           <div>Hi my name is Jakub Sznajder. I am Front-End Developer looking for internship / job in Krakow. I'm positive about new challenges. I learn quickly and contantly develop myself. My hobbies are cryptocurrency trading, music and programming of course.</div>
+          
          <h2>My Abilities</h2>
          <Fade right>
           <ul>
@@ -25,6 +26,7 @@ const Aboutme = () => {
             <li><img src={css} alt=""/></li>
             <li><img src={js} alt=""/></li>
             <li><img src={react} alt=""/></li>
+            <li><img src={redux} alt=""/></li>
             <li><img src={nodejs} alt=""/></li>
             <li><img src={postgres} alt=""/></li>
             <li><img src={illustrator} alt=""/></li>
