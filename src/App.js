2bf78025e41d0b5665156090d24dd6a32b71ec6f
Jakub Sznajder <donatelek92@gmail.com>
2019-07-31 21:31:14 +0200

    add new icons to about me

--- src/App.js
+++ src/App.js
@@ -4,93 +4,93 @@ import Main from './pages/Main';
 import Projects from './pages/Projects';
 import Aboutme from './pages/Aboutme';
 import Footer from './pages/Footer';
-import './styles/App.css';
+import './styles/style.css';
 
 class App extends Component {
-  state = { 
-    isTop:'onTop',
-    page:'main'
-   }
-   componentDidMount(){
+  state = {
+    isTop: 'onTop',
+    page: 'main'
+  }
+  componentDidMount() {
     document.addEventListener('load', () => {
       const isTop = window.scrollY < 580;
       if (isTop !== this.state.isTop) {
-        if(isTop===true){
+        if (isTop === true) {
           this.setState({
-            isTop:'onTop'
-           })
-        }else{
+            isTop: 'onTop'
+          })
+        } else {
           this.setState({
-            isTop:'onBottom'
-           })
+            isTop: 'onBottom'
+          })
         }
       }
     });
     document.addEventListener('scroll', () => {
       const isTop = window.scrollY < 160;
-      if(this.state.page!=='project'){
+      if (this.state.page !== 'project') {
         if (isTop !== this.state.isTop) {
-          if(isTop===true){
+          if (isTop === true) {
             this.setState({
-              isTop:'onTop'
-             })
-          }else{
+              isTop: 'onTop'
+            })
+          } else {
             this.setState({
-              isTop:'onBottom'
-             })
+              isTop: 'onBottom'
+            })
           }
         }
       }
     });
   }
-  
-  handleShowHamburger=()=>{
-    this.setState({
-        showHamburger:!this.state.showHamburger
-    })
-    }
 
-handleChangePage=page=>{
-  if(page==='project'){
+  handleShowHamburger = () => {
     this.setState({
-      isTop:'hidden'
+      showHamburger: !this.state.showHamburger
     })
-  }else{
-    document.body.style.overflow = "visible"
-    const isTop = window.scrollY < 160;
-    if(isTop===true){
-      this.setState({
-        isTop:'onTop'
-      })
-    }else{
-      this.setState({
-        isTop:'onBottom'
-      })
-    }
   }
-  if(page==='project'||page==='nav'){
-    document.body.style.overflow = "hidden"
-  }
-  this.setState({
-    page
-  })
-}
 
-  render() { 
-    return ( 
+  handleChangePage = page => {
+    if (page === 'project') {
+      this.setState({
+        isTop: 'hidden'
+      })
+    } else {
+      document.body.style.overflow = "visible"
+      const isTop = window.scrollY < 160;
+      if (isTop === true) {
+        this.setState({
+          isTop: 'onTop'
+        })
+      } else {
+        this.setState({
+          isTop: 'onBottom'
+        })
+      }
+    }
+    if (page === 'project' || page === 'nav') {
+      document.body.style.overflow = "hidden"
+    }
+    this.setState({
+      page
+    })
+  }
+
+  render() {
+    return (
       <>
-      <Nav handleChangePage={this.handleChangePage} page={this.state.page} isTop={this.state.isTop}/>
-      <div style={{overflowX:'hidden'}}>
-      <Main/>
-      <Projects handleChangePage={this.handleChangePage} page={this.state.page} />
-      <Aboutme/>
-      <Footer/>
-      </div>
+        <Nav handleChangePage={this.handleChangePage} page={this.state.page} isTop={this.state.isTop} />
+        <div style={{ overflowX: 'hidden' }}>
+          <Main />
+          <Projects handleChangePage={this.handleChangePage} page={this.state.page} />
+          <Aboutme />
+          <Footer />
+        </div>
       </>
-     );
+    );
   }
 }
- 
+
 export default App;
 
 
